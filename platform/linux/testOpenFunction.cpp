f76154a7e284937e13346260c5988e3e07a74e93
jc <jc@forzenHeart.(none)>
2015-12-16 17:54:41 +0800

    make sure the path argument about the LinuxPlatform of theOpen() Function

--- platform/linux/testOpenFunction.cpp
+++ platform/linux/testOpenFunction.cpp
@@ -4,31 +4,40 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-
+//  /dev/mapper/vg_forzenheart-lv_root
+//  /dev/sda1
+// /dev/mapper/vg_forzenheart-lv_home
+// scsi-SATA_ST1000DM003-1ER_W4Y5TGGF
+// scsi-SATA_ST1000DM003-1ER_W4Y5TGGF-part1
+// /dev/disk/by-id
 int
 main(void)
 {
-	int	DiskFd;
 	int	n;
-	unsigned char	DiskBuf[512];
+	int	DiskFd;
 	off_t	currpos;
+	unsigned char	DiskBuf[512];
 
-	if ((DiskFd = open("/dev/sda", O_RDONLY)) < 0) 
+	if ((DiskFd = open("/dev/sda", O_RDONLY)) < 0)
 		fprintf(stderr, "open error\n");
-	//currpos = lseek(DiskFd, 0, SEEK_CUR);
-	//printf("off_t now at : %d\n", currpos);
+	else {
+		currpos = lseek(DiskFd, 0, SEEK_CUR);
+		printf("off_t now at : %lu\n", currpos);
 
-	if ((n = read(DiskFd, DiskBuf, sizeof(DiskBuf))) < 0) 
-		fprintf(stderr, "read error\n");
-	printf("read : %d bytes.\nThe Disk info:\n", n);
+		if ((n = read(DiskFd, DiskBuf, sizeof(DiskBuf))) < 0) 
+			fprintf(stderr, "read error\n");
+		printf("read : %d bytes.\nThe Disk info:\n", n);
 
-	for (int i = 0; i < 32; i++)
-	{
-		for (int j = 0; j < 16; j++)
+		for (int i = 0; i < 32; i++)
 		{
-			printf("%02x ", DiskBuf[i * 32 + j]);
+			printf("%d : ",i);
+			for (int j = 0; j < 16; j++)
+			{
+				printf("%02x ", DiskBuf[i * 32 + j]);
+			}
+			printf("\n");
 		}
-		printf("\n");
+
 	}
 
 	close(DiskFd);
