09c4ed650477e2cd0ee8b83b7485dc7c04c97b24
jc <jc@forzenHeart.(none)>
2015-12-17 12:02:57 +0800

    fix wrong information of MBR

--- platform/linux/MBR_linux.cpp
+++ platform/linux/MBR_linux.cpp
@@ -10,34 +10,42 @@
 // scsi-SATA_ST1000DM003-1ER_W4Y5TGGF
 // scsi-SATA_ST1000DM003-1ER_W4Y5TGGF-part1
 // /dev/disk/by-id
+
+void print_now_off_set(int files)
+{
+	off_t	currpos;
+	currpos = lseek(files, 0, SEEK_CUR);
+	printf("off_t now at : %lu\n", currpos);
+}
 int
 main(void)
 {
 	int	n;
 	int	DiskFd;
-	off_t	currpos;
-	unsigned char	DiskBuf[512];
+
+	//In Linux, number 4096 keep file system read or write run at peak efficicency
+	unsigned char	DiskBuf[4096];
 
 	if ((DiskFd = open("/dev/sda", O_RDONLY)) < 0)
-		fprintf(stderr, "open error\n");
+		fprintf(stderr, "open error.This programma"
+				"needs to be run from root! Make sure about that.\n");
 	else {
-		currpos = lseek(DiskFd, 0, SEEK_CUR);
-		printf("off_t now at : %lu\n", currpos);
-
+		print_now_off_set(DiskFd);
 		if ((n = read(DiskFd, DiskBuf, sizeof(DiskBuf))) < 0) 
 			fprintf(stderr, "read error\n");
 		printf("read : %d bytes.\nThe Disk info:\n", n);
-
+		print_now_off_set(DiskFd);
 		for (int i = 0; i < 32; i++)
 		{
-			printf("%d : ",i);
+			//Change the print format of decimal or hex
+			//printf("%.11d : ",i * 16);
+			printf("%011X : ",i * 16);
 			for (int j = 0; j < 16; j++)
 			{
-				printf("%02x ", DiskBuf[i * 32 + j]);
+				printf("%02X ", DiskBuf[i * 16 + j]);
 			}
 			printf("\n");
 		}
-
 	}
 
 	close(DiskFd);
