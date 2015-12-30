7c620e03bfd403b5b466a80b9da9b61706b33650
Easeus <junchenghe@outlook.com>
2015-12-30 18:00:32 +0800

    remove the open close function to CPartitionList Class

--- ReadPartition/FATPhysicalDisk.h
+++ ReadPartition/FATPhysicalDisk.h
@@ -5,10 +5,6 @@ class CPhysicalDisk {
 	public:
 		virtual		INT64	ReadSector();
 		virtual		INT64	WriteSector();
-	private:
-		virtual	INT64	seek(INT64 offset);
-		virtual	int	open();
-		virtual	int	close();
 };
 
 #endif	//_FAT_PHYSICAL_DISK_H__
