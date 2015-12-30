3ba4e0a186e75ed99459ffd16ad5355e71502e31
Easeus <junchenghe@outlook.com>
2015-12-30 17:57:27 +0800

    not sure about the disk Class

--- ReadPartition/FATPartitionListImpl.h
+++ ReadPartition/FATPartitionListImpl.h
@@ -5,11 +5,12 @@
 
 class CPartitionListImpl {
 	public:
-	virtual	int	EnumPhysicalDisk() = 0;
-	
-	int	GetPhysicalDiskCount() const;
-	CPhysicalDisk*	GetPhysicalDiskByID(const int which) const;
-	const std::string*	GetPhysicalDiskNameByID(const int which) const;
+		//enum the physical information on different platform
+		virtual	int	EnumPhysicalDisk() = 0;
+
+		int	GetPhysicalDiskCount() const;
+		CPhysicalDisk*	GetPhysicalDiskByID(const int which) const;
+		const std::string*	GetPhysicalDiskNameByID(const int which) const;
 	private:
 		std::vector<CPhysicalDisk>	m_physicalDiskList;
 };
--- ReadPartition/FATPhysicalDisk.h
+++ ReadPartition/FATPhysicalDisk.h
@@ -2,8 +2,13 @@
 #define	_FAT_PHYSICAL_DISK_H__
 
 class CPhysicalDisk {
-
+	public:
+		virtual		INT64	ReadSector();
+		virtual		INT64	WriteSector();
 	private:
+		virtual	INT64	seek(INT64 offset);
+		virtual	int	open();
+		virtual	int	close();
 };
 
 #endif	//_FAT_PHYSICAL_DISK_H__
