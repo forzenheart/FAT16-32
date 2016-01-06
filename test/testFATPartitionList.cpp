c64c13dc102d7a59ebec161802bde33231e41083
Easeus <junchenghe@outlook.com>
2016-01-06 15:48:56 +0800

    test fail

--- test/testFATPartitionList.cpp
+++ test/testFATPartitionList.cpp
@@ -1,16 +1,20 @@
-//#include "ReadPartition/FATPartitionList.h"
+#include "ReadPartition/FATPartitionList.h"
 #include <stdio.h>
 
 int main(void)
 {
 	
-	//CPartitionList*	cp = CPartitionList::GetInstance();
-	//cp->EnumPartition();
+	CPartitionList*	cp = CPartitionList::GetInstance();
 
-	printf("No AcFlag  Capacity  TotalSec  SecPre  TypeName  StartHead  StartSec  StartCylinder  StartHead  StartSec  StartCylinder\n");
-	//for (int i = 0; i < cp->GetPartitionCount(); i++)
-	//{
-		//printf("%s\n");
-	//}
+	printf("No	AcFlag	Capacity(MB)	TypeName\n");
+	for (int i = 0; i < cp->GetPartitionCount(); i++)
+	{
+		printf("%d	%s	 %d	%s\n", 
+				i, 
+				cp->GetActiveFlagByID(i) ? "Yes" : "No", 
+				cp->GetTotalSecByID(i) * 512, 
+				cp->GetPartitionTypeByID(i));
+
+	}
 	return 0;
 }
